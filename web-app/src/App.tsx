3f897327dac42b542b96676f8fdbe5555ad43bac
Sandro Salzmann <sandro.salzmann@stud.hslu.ch>
2022-02-19 02:09:55 +0100

    Add divider

--- web-app/src/App.tsx
+++ web-app/src/App.tsx
@@ -1,16 +1,17 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Container } from "semantic-ui-react";
+import { Container, Divider } from "semantic-ui-react";
 import { Header } from "./components/Header";
 import { AdminPage } from "./pages/AdminPage";
-import { ViewerPage } from "./pages/ViewerPage";
 import { NotFoundPage } from "./pages/NotFoundPage";
+import { ViewerPage } from "./pages/ViewerPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Container text>
         <Header />
+        <Divider />
         <Routes>
           <Route path="" element={<ViewerPage />} />
           <Route path="admin" element={<AdminPage />} />
