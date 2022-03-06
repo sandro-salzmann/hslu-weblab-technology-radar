d09b744cda39551e3c6531e80c56d5317e057d52
Sandro Salzmann <salzmann2k@gmail.com>
2022-03-06 01:49:53 +0100

    Add technology service initial db schema

---
+++ technology-service/db/schema.sql
@@ -0,0 +1,20 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+DROP TABLE account technology CASCADE;
+
+DROP TYPE category;
+DROP TYPE maturity;
+
+CREATE TYPE category AS ENUM ('techniques', 'platforms', 'tools', 'languages');
+
+CREATE TYPE maturity AS ENUM ('assess', 'trial', 'adopt', 'hold');
+
+CREATE TABLE technology (
+    id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
+    team_id uuid NOT NULL,
+    category category,
+    maturity maturity,
+    name varchar,
+    description varchar,
+    descriptionClassification varchar
+);
