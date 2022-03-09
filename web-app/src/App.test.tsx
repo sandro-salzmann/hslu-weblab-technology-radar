aba6a1510585121c0aef98fcb198da16aaa7f7fd
Sandro Salzmann <salzmann2k@gmail.com>
2022-03-09 17:11:37 +0100

    Remove unused test

--- web-app/src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders header title', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/📡 Techradar/i);
-  expect(linkElement).toBeInTheDocument();
-});
